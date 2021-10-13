4325f954e82b6d8d8fba9294cbab591626e6815e
Oleg Rakovitch <devgeslot@gmail.com>
2021-10-13 18:04:17 -0400

    Initial implementation

---
+++ TryReturn/AsyncTryExpression.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Staticsoft.TryReturn
+{
+    public class AsyncTryExpression<TTask>
+    {
+        readonly Task<TTask> Task;
+        readonly IEnumerable<ExceptionHandler> Handlers;
+
+        public AsyncTryExpression(Task<TTask> task)
+            : this(task, Enumerable.Empty<ExceptionHandler>()) { }
+
+        AsyncTryExpression(Task<TTask> task, IEnumerable<ExceptionHandler> handlers)
+            => (Task, Handlers) = (task, handlers);
+
+        public async Task<TTask> Result()
+        {
+            try
+            {
+                return await Task;
+            }
+            catch (Exception exception)
+            {
+                var exceptionType = exception.GetType();
+                foreach (var handler in Handlers)
+                {
+                    if (handler.ExceptionType.IsAssignableFrom(exceptionType))
+                    {
+                        throw handler.Handle(exception);
+                    }
+                }
+                throw;
+            }
+        }
+
+        public AsyncTryExpression<TTask> On<T>(Func<T, Exception> handler) where T : Exception
+            => On<TTask>(ex => handler(ex as T), typeof(T));
+
+        AsyncTryExpression<TTask> On<T>(Func<Exception, Exception> handler, Type exceptionType)
+            => new(Task, Handlers.Append(new ExceptionHandler(handler, exceptionType)));
+    }
+}
---
+++ TryReturn/TryExpression.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Staticsoft.TryReturn
+{
+    public class TryExpression<TExpression>
+    {
+        readonly Func<TExpression> Func;
+        readonly IEnumerable<ExceptionHandler> Handlers;
+
+        public TryExpression(Func<TExpression> func)
+            : this(func, Enumerable.Empty<ExceptionHandler>()) { }
+
+        TryExpression(Func<TExpression> func, IEnumerable<ExceptionHandler> handlers)
+            => (Func, Handlers) = (func, handlers);
+
+        public TExpression Result()
+        {
+            try
+            {
+                return Func.Invoke();
+            }
+            catch (Exception exception)
+            {
+                var exceptionType = exception.GetType();
+                foreach (var handler in Handlers)
+                {
+                    if (handler.ExceptionType.IsAssignableFrom(exceptionType))
+                    {
+                        throw handler.Handle(exception);
+                    }
+                }
+                throw;
+            }
+        }
+
+        public TryExpression<TExpression> On<T>(Func<T, Exception> handler) where T : Exception
+            => On<TExpression>(ex => handler(ex as T), typeof(T));
+
+        TryExpression<TExpression> On<T>(Func<Exception, Exception> handler, Type exceptionType)
+            => new(Func, Handlers.Append(new ExceptionHandler(handler, exceptionType)));
+    }
+}
